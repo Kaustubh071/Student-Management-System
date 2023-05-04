065f47d57dd1ef658de9919fe1c753941fea098f
Kaustubh Sathawane <131289678+Kaustubh071@users.noreply.github.com>
2023-05-04 21:09:42 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -7,20 +7,29 @@ struct student{
 };
 int main()
 {
-    int i,n=0,ch=0,m=0,l=1,temp,j,c,k,o=0,rollno;
-    char temp1[20],name[25];
+    int i=0,n=0,ch=0,m=0,l=1,temp,j=1,c,rollno,d=0,x=0,e=0;
+    char temp1[20],NAME[25];
+    struct student st[20];
     FILE *fp;
+    fp=fopen("student.txt","r");
 
-
-    struct student st[5];
-    fp=fopen("myfile.txt","r");
-    for(i=0;i<m;i++)
-                  {
-                      fscanf(fp,"\n%d\t\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",&st[i].roll,&st[i].name,&st[i].p1,&st[i].p2,&st[i].p3,&st[i].p1+st[i].p2+st[i].p3);
-
-                  }
-    fclose(fp);
-
+    //Checking file empty or not
+    fseek(fp,0,SEEK_END);
+    if(ftell(fp)==0)
+    {
+        printf("No record\n");
+    }
+    fseek(fp,0,SEEK_SET);
+    if(ftell(fp)!=0)
+    {
+        //end
+    while(!feof(fp))
+    {
+       fscanf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t",&st[i].roll,&st[i].name,&st[i].p1,&st[i].p2,&st[i].p3);
+       i++;
+    }
+    }
+    m=i;
     printf("Welcome to Student Management System\n");
        while(l){
     printf("1. Add Student\n");
@@ -38,12 +47,23 @@ int main()
                 printf("Number of student : ");
           scanf("%d",&n);
 
-          for(i=m,k=0;i<m+n,k<n;i++,k++)
+          for(i=m;i<m+n;i++)//2
           {
 
-              printf("Student %d\n",k+1);
+              printf("Student %d\n",i+1);
               printf("\nRoll number: ");
-              scanf("%d",&st[i].roll);
+             /* scanf("%d",&rollno);
+              for(x=0;x<i;x++)
+              {
+                  if(rollno!=st[x].roll)
+                  {
+                    e++;
+                  }
+
+              }
+              if(e==i)
+              {*/
+              st[i].roll=rollno;
               printf("\nName: ");
               scanf("%s",&st[i].name);
               fflush(stdin);
@@ -54,55 +74,36 @@ int main()
               scanf("%d",&st[i].p2);
               printf("\nPaper3: ");
               scanf("%d",&st[i].p3);
-              fp = fopen("myfile.txt","a");
-              fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
-              fclose(fp);
+              //}
+              //else{
+              //  printf("\nRoll number is already used");
+              //}
 
           }
          m+=n;
         printf("Record Save..!\n");
-        /*fp = fopen("myfile.txt","a");
-        for(i=m;i<m+n;i++)
-        fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
-        fclose(fp);*/
+
+        fp=fopen("student.txt","w");
+        for(i=0;i<m;i++)
+        {
+            fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+        }
+        fclose(fp);
+
             break;
             case 2:
-           /*
-           fp=fopen("myfile.txt","r");
-            printf("RollNo\t|\tName\t|\tPaper1\t|\tPaper2\t|\tPaper3\t|\tResult\t|\tRemark\n");
-           i=0;
-            while(!feof(fp))
-            {
-
-                  fscanf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",&st[i].roll,&st[i].name,&st[i].p1,&st[i].p2,&st[i].p3,&st[i].p1+st[i].p2+st[i].p3);
-                  i++;
-            }
+            printf("Roll number\t|\tName\t|\tPaper1\t|\tPaper2\t|\tPaper3\t|\tResult\t|\tRemark\n");
             for(i=0;i<m;i++)
             {
-                printf("\n%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                if(st[i].roll!=0)
+                {
+                 printf("\n%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
                 if((st[i].p1+st[i].p2+st[i].p3)>100)
                 printf("Pass\n");
                 else
                 printf("Fail\n");
+                }
             }
-            */
-
-            fp=fopen("myfile.txt","r");
-            printf("RollNo\t|\tName\t|\tPaper1\t|\tPaper2\t|\tPaper3\t|\tResult\t|\tRemark\n");
-
-           while(!feof(fp))
-           {
-            fscanf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",&st[i].roll,&st[i].name,&st[i].p1,&st[i].p2,&st[i].p3,&st[i].p1+st[i].p2+st[i].p3);
-            printf("\n%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
-                if((st[i].p1+st[i].p2+st[i].p3)>100)
-                printf("Pass\n");
-                else
-                printf("Fail\n");
-           }
-
-          fclose(fp);
-
-
             break;
             case 3:
                 printf("Delete Record :\n");
@@ -114,52 +115,64 @@ int main()
                   {
               printf("\nRoll number: %d",st[i].roll);
               printf("\nName: %s",st[i].name);
-                  }
-              }
-              for(i=0;i<m;i++)
-              {
-                   if(temp==st[i].roll)
-                   {printf("Do you really want to delete this record? (y/n) \n");
-              printf("Choice:");
-              scanf("%d",&c);
-           if(c==1)
-           {
-              for(i=0;i<m;i++)
-              {
-                if(temp==st[i].roll)
-                {
-                  for(j=i;j<m;j++)
-                  {
-                      st[j]=st[j+1];
-                  }
-                  m--;
-                }
-              }
 
-              printf("...OK RECORD DELETED\n");
-
-              printf("Roll number\t|\tName\t|\tPaper1\t|\tPaper2\t|\tPaper3\t|\tResult\t|\tRemark\n");
+              while(j)
+              {
+                printf("Do you really want to delete this record? (y/n) \n");
+                printf("Choice:");
+                scanf("%d",&c);
+                 if(c==1)
+                 {
+                     strcpy(st[i].name," ");
+                  st[i].roll = 0;
+                  st[i].p1 = 0;
+                  st[i].p2 = 0;
+                  st[i].p3 = 0;
+                  printf("...OK RECORD DELETED\n");
+                  j=0;
+                   fp=fopen("student.txt","w");
                  for(i=0;i<m;i++)
-          {
-              printf("%d\t\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t|\t",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
-
-             if((st[i].p1+st[i].p2+st[i].p3)>100)
-                printf("Pass\n");
-              else
-                printf("Fail\n");
-          }
-           }
-           else if(c==2)
-
-              printf("....OK NO RECORD DELETED\n");
-                    else
-                    printf("this number is not in roll list!!!\n");
+                 {
+                     if(st[i].roll!=0)
+                    fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
                    }
+                fclose(fp);
+                        for(i=0;i<m;i++)
+          {
 
+              if(st[i].roll!=0)
+                {
+                 printf("\n%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\t",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                if((st[i].p1+st[i].p2+st[i].p3)>100)
+                printf("Pass\n");
+                else
+                printf("Fail\n");
+                }
+          }
 
+                 }
+                 else if(c==2)
+                 {
+                     printf("....OK NO RECORD DELETED\n");
+                     j=0;
+                 }
+                 else
+
+                     printf("Enter right choice please!!\n");
+                 }
               }
+              else{
+                d++;
+              }
+                  }
+                  if(m==d)
+                  {
+                      printf("No record found !!\n");
+                  }
+                  d=0;
+                  j=1;
+                  break;
 
-             break;
 
 
             case 4:
@@ -168,6 +181,7 @@ int main()
                    printf("Update Record\n");
                 printf("Enter student roll : ");
                 scanf("%d",&temp);
+
 
                 for(i=0;i<m;i++)
                 {
@@ -185,49 +199,141 @@ int main()
                             if(temp==st[i].roll)
                             {
                                 printf("Roll no %d Current name is %s\nEnter new Name : ",st[i].roll,st[i].name);
-                                scanf("%s",st[i].name);
-
-                            }
-                        }
-                        }
-
-                    else if(c==2)
-                    {
-                      for(i=0;i<m;i++)
-                        {
-                            if(temp==st[i].roll)
-                            {
-                                printf("Roll no %d Current paper1 marks are  %d\nEnter new paper1 marks : ",st[i].roll,st[i].p1);
-                                scanf("%d",&st[i].p1);
-                            }
-                        }
-                    }
-                    else if(c==3)
-                    {
+                                scanf("%s",NAME);
+                                printf("Do you really want to change the name \n Choice:");
+                                scanf("%d",&c);
+                                if(c==1)
+                                {
+                                    strcpy(st[i].name,NAME);
+                                    printf("...OK RECORD UPDATED\n");
+                                    fp=fopen("student.txt","w");
                         for(i=0;i<m;i++)
-                        {
-                            if(temp==st[i].roll)
-                            {
-                                printf("Roll no %d Current paper2 marks are  %d\nEnter new paper2 marks : ",st[i].roll,st[i].p2);
-                                scanf("%d",&st[i].p2);
-                            }
-                        }
-                    }
-                  else
-                    {
-                    for(i=0;i<m;i++)
-                        {
-                            if(temp==st[i].roll)
-                            {
-                                printf("Roll no %d Current paper3 marks are  %d\nEnter new paper3 marks : ",st[i].roll,st[i].p3);
-                                scanf("%d",&st[i].p3);
-                            }
-                        }
-                  }
+                       {
+                           fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                           }
+                      fclose(fp);
 
-                    }
-                    }
+
+                                }
+                                else
+                                     printf("...OK RECORD NOT UPDATED\n");
+                            }
+
+                        }
+
+
+
                 }
+
+                 else if(c==2)
+                        {
+                          for(i=0;i<m;i++)
+                        {
+                            if(temp==st[i].roll)
+                            {
+                                printf("Do you really want to change the Marks \n Choice:");
+                                scanf("%d",&c);
+                                if(c==1)
+                                {
+                                    printf("Roll no %d Current paper1 marks are %d\nEnter new marks : ",st[i].roll,st[i].p1);
+                                    scanf("%d",&st[i].p1);
+                                     fp=fopen("student.txt","w");
+                                   for(i=0;i<m;i++)
+                           {
+                           fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                           }
+                      fclose(fp);
+                                    printf("...OK RECORD UPDATED\n");
+
+
+
+                                }
+                                else
+                                     printf("...OK RECORD NOT UPDATED\n");
+                            }
+
+                        }
+
+                }
+
+
+                else if(c==3)
+                        {
+                          for(i=0;i<m;i++)
+                        {
+                            if(temp==st[i].roll)
+                            {
+                                printf("Do you really want to change the Marks \n Choice:");
+                                scanf("%d",&c);
+                                if(c==1)
+                                {
+                                    printf("Roll no %d Current paper2 marks are %d\nEnter new marks : ",st[i].roll,st[i].p2);
+                                    scanf("%d",&st[i].p2);
+                                     fp=fopen("student.txt","w");
+                                   for(i=0;i<m;i++)
+                           {
+                           fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                           }
+                      fclose(fp);
+                                    printf("...OK RECORD UPDATED\n");
+
+
+
+                                }
+                                else
+                                     printf("...OK RECORD NOT UPDATED\n");
+                            }
+
+                        }
+
+                }
+
+                else if(c==4)
+                        {
+                          for(i=0;i<m;i++)
+                        {
+                            if(temp==st[i].roll)
+                            {
+                                printf("Do you really want to change the Marks \n Choice:");
+                                scanf("%d",&c);
+                                if(c==1)
+                                {
+                                    printf("Roll no %d Current paper3 marks are %d\nEnter new marks : ",st[i].roll,st[i].p3);
+                                    scanf("%d",&st[i].p3);
+                                     fp=fopen("student.txt","w");
+                                   for(i=0;i<m;i++)
+                           {
+                           fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                           }
+                      fclose(fp);
+                                    printf("...OK RECORD UPDATED\n");
+
+
+
+                                }
+                                else
+                                     printf("...OK RECORD NOT UPDATED\n");
+                            }
+
+                        }
+
+                }
+        }
+
+
+
+
+                else{
+                       d++;
+                    }
+
+        }
+         if(m==d) {
+                    printf("No Record found!!\n");
+                  }
+                d=0;
+
+       }
 
 
              break;
@@ -300,10 +406,34 @@ int main()
 
                 l=0;
                 if(temp==2)
-                break;
+                {
+                     for(i=0;i<m;i++)
+                   {
+                    strcpy(st[i].name," ");
+                  st[i].roll = 0;
+                  st[i].p1 = 0;
+                  st[i].p2 = 0;
+                  st[i].p3 = 0;
+                   }
+
+                    fp=fopen("student.txt","w");
+                    for(i=0;i<m;i++)
+                   {
+                       if(st[i].roll!=0)
+                     fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                    }
+                     fclose(fp);
+                    printf("THANK YOU!\nFOR USING SYSTEM");
+                }
 
                 if(temp==1)
                 {
+                     fp=fopen("student.txt","w");
+                    for(i=0;i<m;i++)
+                   {
+                     fprintf(fp,"%d\t|\t%s\t|\t%d\t|\t%d\t|\t%d\t|\t%d\t\n",st[i].roll,st[i].name,st[i].p1,st[i].p2,st[i].p3,st[i].p1+st[i].p2+st[i].p3);
+                    }
+                     fclose(fp);
                     printf("THANK YOU!\nFOR USING SYSTEM");
                 }
 
